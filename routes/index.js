64f183b5000edd3d905b6a3e226cfc39563f3ec5
Jorge González <jorge.gonzalez@babel.es>
2016-04-22 13:57:09 +0200

    Añadiendo al carrito los items.

--- routes/index.js
+++ routes/index.js
@@ -15,120 +15,148 @@ var items = {
     'humor': [{
         title: 'libro1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro2',
         author: 'autor2',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro3',
         author: 'autor3',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro4',
         author: 'autor4',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro5',
         author: 'autor5',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro6',
         author: 'autor6',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro7',
         author: 'autor7',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro8',
         author: 'autor8',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ],
     'suspense': [{
         title: 'libro1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'libro1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ],
     'accion': [{
         title: 'peli1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ],
     'drama': [{
         title: 'peli2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'peli2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ],
     'moviles': [{
         title: 'movil2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'movil2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'movil2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'movil2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ],
     'portatiles': [{
         title: 'portatil1',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'portatil2',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'portatil3',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, {
         title: 'portatil4',
         author: 'autor1',
-        cover: ''
+        cover: 'https://placeholdit.imgix.net/~text?txtsize=20&bg=4682b4&txtclr=000000&txt=item&w=150&h=150',
+        price: '22'
     }, ]
 };
 
